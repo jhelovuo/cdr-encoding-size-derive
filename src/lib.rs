86c85cd176cf4b656e4fb9dd16beadb9c9b4edc9
Juhana Helovuo <juhana.helovuo@atostek.com>
2021-11-01 16:02:57 +0200

    README. Comments.

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,11 @@ use quote::{quote, quote_spanned};
 use syn::spanned::Spanned;
 use syn::{parse_macro_input, parse_quote, Data, DeriveInput, Fields, GenericParam, Generics};
 
+// This is derived form the HeapSize example in syn crate documentation.
+// the main difference are
+// * This works on maximum size of data types, whereas the original example counts data item sizes.
+// * different naming
+
 #[proc_macro_derive(CdrEncodingSize)]
 pub fn derive_cdr_encoding_size(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     // Parse the input tokens into a syntax tree.
@@ -11,11 +16,11 @@ pub fn derive_cdr_encoding_size(input: proc_macro::TokenStream) -> proc_macro::T
     // Used in the quasi-quotation below as `#name`.
     let name = input.ident;
 
-    // Add a bound `T: HeapSize` to every type parameter T.
+    // Add a bound `T: CdrEncodingSize` to every type parameter T.
     let generics = add_trait_bounds(input.generics);
     let (impl_generics, ty_generics, where_clause) = generics.split_for_impl();
 
-    // Generate an expression to sum up the heap size of each field.
+    // Generate an expression to sum up the size of each field.
     let sum = cdr_size_sum(&input.data);
 
     let expanded = quote! {
@@ -31,7 +36,7 @@ pub fn derive_cdr_encoding_size(input: proc_macro::TokenStream) -> proc_macro::T
     proc_macro::TokenStream::from(expanded)
 }
 
-// Add a bound `T: HeapSize` to every type parameter T.
+// Add a bound `T: CdrEncodingSize` to every type parameter T.
 fn add_trait_bounds(mut generics: Generics) -> Generics {
     for param in &mut generics.params {
         if let GenericParam::Type(ref mut type_param) = *param {
@@ -47,18 +52,7 @@ fn cdr_size_sum(data: &Data) -> TokenStream {
         Data::Struct(ref data) => {
             match data.fields {
                 Fields::Named(ref fields) => {
-                    // Expands to an expression like
-                    //
-                    //     0 + self.x.heap_size() + self.y.heap_size() + self.z.heap_size()
-                    //
-                    // but using fully qualified function call syntax.
-                    //
-                    // We take some care to use the span of each `syn::Field` as
-                    // the span of the corresponding `heap_size_of_children`
-                    // call. This way if one of the field types does not
-                    // implement `HeapSize` then the compiler's error message
-                    // underlines which field it is. An example is shown in the
-                    // readme of the parent directory.
+
                     let recurse = fields.named.iter().map(|f| {
                         let ty = &f.ty;
                         quote_spanned! {f.span()=>
@@ -70,9 +64,6 @@ fn cdr_size_sum(data: &Data) -> TokenStream {
                     }
                 }
                 Fields::Unnamed(ref fields) => {
-                    // Expands to an expression like
-                    //
-                    //     0 + self.0.heap_size() + self.1.heap_size() + self.2.heap_size()
                     let recurse = fields.unnamed.iter().enumerate().map(|(_i, f)| {
                         let ty = &f.ty;
                         //let index = Index::from(i);
@@ -85,7 +76,6 @@ fn cdr_size_sum(data: &Data) -> TokenStream {
                     }
                 }
                 Fields::Unit => {
-                    // Unit structs cannot own more than 0 bytes of heap memory.
                     quote!(cdr_encoding_size::CdrEncodingMaxSize::Bytes(0))
                 }
             }
